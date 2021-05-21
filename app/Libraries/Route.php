c562f14b45b8c6e9af025730c23fae2ea86af52d
DayaneCordeiro <dayane.cordeirogs@gmail.com>
2021-05-21 20:15:39 -0300

    [Feature] Including - URL Function to Recover and Filter URLs

--- app/Libraries/Route.php
+++ app/Libraries/Route.php
@@ -2,6 +2,18 @@
 
 class Route {
     public function __construct() {
-        echo "test";
+        $this->url();
+    }
+
+    /// @brief Recover and Filter URL
+    public function url() {
+        $url = filter_input(INPUT_GET, 'url', FILTER_SANITIZE_URL); // Validating URL - removing bad characters
+
+        if (isset($url)) {
+            $url = trim(rtrim($url, '/'));                          // Remove blank spaces of the start and the end of the string
+            $url = explode('/', $url);
+
+            return $url;
+        }
     }
 }
