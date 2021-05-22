0ee0254f7cfe17a6dabedb08554190747c88189a
DayaneCordeiro <dayane.cordeirogs@gmail.com>
2021-05-22 19:53:51 -0300

    [BugFix] Editing - Config File

--- app/config.php
+++ app/config.php
@@ -1,6 +1,5 @@
 <?php
-
-const APP         = dirname(__FILE__);
+define('APP', dirname(__FILE__));
 const URL         = 'http://localhost/projects/PHP_MVC/';
 const APP_NAME    = 'PHP OO MVC';
 const APP_VERSION = '1.0.0';
--- public/index.php
+++ public/index.php
@@ -11,7 +11,7 @@ include '../app/Libraries/Controller.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP MVC</title>
+    <title><?=APP_NAME?></title>
 </head>
 <body>
     <?php
