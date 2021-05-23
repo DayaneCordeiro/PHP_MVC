2061c7a867aaea591915b3a47c5a9fa6fd5a492c
DayaneCordeiro <dayane.cordeirogs@gmail.com>
2021-05-23 18:49:15 -0300

    [Feature] Including - Project Header

---
+++ app/Views/footer.php
@@ -0,0 +1,11 @@
+<footer class="bg-dark p-5 text-light">
+    <div class="container">
+        <small>
+            PHP OO with MVC Course. Version <?= APP_VERSION ?>
+            <div class="border-top mt-3">
+                &COPY; 2021 - <?= date('Y') ?><br>
+                Made with ❤ by Dayane Cordeiro
+            </div>
+        </small>
+    </div>
+</footer>
