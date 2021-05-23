cf4f5e8ad8ce9639c69bc738c9af4b00675d0cba
DayaneCordeiro <dayane.cordeirogs@gmail.com>
2021-05-23 14:55:25 -0300

    [Feature] Including - Bootstrap CDNs and Own CSS and JS Files

--- public/index.php
+++ public/index.php
@@ -12,10 +12,20 @@ include '../app/Libraries/Controller.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?=APP_NAME?></title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <!-- End Bootstrap -->
+
+    <link rel="stylesheet" href="<?=URL?>/public/css/styles.css">
 </head>
 <body>
     <?php
         $routes = new Route();
     ?>
+    <script src="<?=URL?>/public/js/scripts.js"></script>
 </body>
 </html>
