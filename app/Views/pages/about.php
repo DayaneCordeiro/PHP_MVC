40bb270ad32d75872c035f0d7669edcdc41ccaab
DayaneCordeiro <dayane.cordeirogs@gmail.com>
2021-05-23 18:52:57 -0300

    [Enhancement] Modifying - About Us Structure

---
+++ app/Views/pages/about.php
@@ -0,0 +1,20 @@
+<div class="container my-3">
+    <div class="card">
+        <div class="card-body">
+            <h1 class="my-3"><?= $dados['tituloPagina'] ?></h1>
+
+            <p><b>What will you learn?</b></p>
+
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">Build a custom MVC Framework from Zero without using any kind of Framework like Laravel, CodeIgniter, Zend etc.</li>
+                <li class="list-group-item">Create custom PDO class to handle interactions with the database</li>
+                <li class="list-group-item">Complete user authentication using OOP and PDO</li>
+                <li class="list-group-item">Validate forms</li>
+                <li class="list-group-item">Domain and hosting concepts, configure and host the project</li>
+                <li class="list-group-item">Create pagination of results</li>
+                <li class="list-group-item">Create reusable classes and auxiliary functions for uploading images, files, date processing, etc.</li>
+            </ul>
+        </div>
+    </div>
+
+</div>
