aedb90e010140e3340fdc93ef0f21b83b4458919
DayaneCordeiro <dayane.cordeirogs@gmail.com>
2021-06-06 15:45:01 -0300

    [Feature] Including - Creating Posts Form

--- app/Controllers/Posts.php
+++ app/Controllers/Posts.php
@@ -10,4 +10,37 @@ class Posts extends Controller {
     public function index() {
         $this->view('posts/index');
     }
+
+    public function register() {
+        $form = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+        if (isset($form)) {
+            $data = array(
+                "title" => trim($form['title']),
+                "text"  => trim(ltrim($form['text']))
+            );
+
+            var_dump($form);
+
+            if (in_array("", $form)) {
+                // VALIDATING TITLE
+                if (empty($form['title']))
+                    $data['title_error'] = 'Title is required.';
+
+                // VALIDATING TEXT
+                if (empty($form['text']))
+                    $data['text_error'] = 'Text is required.';
+            } else {
+                    // EVERYTHING OK
+                    echo "OK";                
+            }
+        } else {
+            $data = array(
+                "title" => "",
+                "text"  => ""
+            );
+        }
+
+        $this->view('posts/register', $data);
+    }
 }
---
+++ app/Views/posts/register.php
@@ -0,0 +1,41 @@
+<div class="col-md-8 mx-auto p-5">
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="<?= URL ?>/posts">Posts</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Register</li>
+        </ol>
+    </nav>
+
+    <div class="card">
+        <div class="card-header bg-secondary text-white">
+            <h2>New Post</h2>
+        </div>
+        <div class="card-body">
+            <form name="form_posts_register" method="POST" action="<?= URL ?>/posts/register" class="mt-4">
+                <div class="form-group">
+                    <label for="title">Title: <sup class="text-danger">*</sup></label>
+                    <input type="text" name="title" value="<?= $data['title'] ?>" class="form-control <?=(isset($data["title_error"])) ? 'is-invalid' : '' ?>">
+                    <div class="invalid-feedback">
+                        <?= isset($data['title_error']) ? $data['title_error'] : null ?>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="title">Text: <sup class="text-danger">*</sup></label>
+                    <textarea class="form-control <?=(isset($data["text_error"])) ? 'is-invalid' : '' ?>" name="text" rows="3">
+                        <?= $data['text'] ?>
+                    </textarea>
+                    <div class="invalid-feedback">
+                        <?= isset($data['text_error']) ? $data['text_error'] : null ?>
+                    </div>
+                </div>
+
+                <divc class="row">
+                    <div class="col-md-2">
+                        <input type="submit" value="Save" class="btn btn-info btn-block">
+                    </div>
+                </div>
+            </form>
+
+        </div>
+    </div>
+</div>
