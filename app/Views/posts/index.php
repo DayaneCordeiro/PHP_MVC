c2428a3d6acded1aa5ab79d8df4f27da4fe942a9
DayaneCordeiro <dayane.cordeirogs@gmail.com>
2021-06-06 11:52:27 -0300

    [Feature] Including - Creating Posts Controller

---
+++ app/Views/posts/index.php
@@ -0,0 +1,13 @@
+<div class="container py-5">
+    <div class="card">
+        <div class="card-header bg-info text-white">
+            POSTS
+            <div class="float-right">
+                <a href="<?= URL ?>/posts/register" class="btn btn-success">Create post</a>
+            </div>
+        </div>
+        <div class="card-body">
+            <p>List posts here.</p>
+        </div>
+    </div>
+</div>
